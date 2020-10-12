a025922a5080c112348c43c65cb3134987c4e0a8
Matthew Rungwe <matthewrungwe@gmail.com>
2020-10-12 14:02:15 +0200

    Add sigup component to routing.

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
+  { path: 'signup', component: SignupComponent }
   // { path: '', redirectTo: '/welcome', pathMatch: 'full' }
 ];
 
