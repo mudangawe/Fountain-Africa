d9ef5804e961069be354df8b8fb75a56fcfebaf3
Matthew Rungwe <matthewrungwe@gmail.com>
2020-10-22 16:45:26 +0200

    Update dashboard.

--- Frontend/src/app/dashboard/dashboard.module.ts
+++ Frontend/src/app/dashboard/dashboard.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
 import { DashboardProfileComponent } from './dashboard-profile/dashboard-profile.component';
 import { DashboardSidebarComponent } from './dashboard-sidebar/dashboard-sidebar.component';
 import { DashboardDisplayComponent } from './dashboard-display/dashboard-display.component';
 
-
+const ROUTES = [
+  { path: 'dashboard', component: DashboardDisplayComponent }
+]
 
 @NgModule({
-  declarations: [DashboardProfileComponent, DashboardSidebarComponent, DashboardDisplayComponent],
+  declarations: [
+    DashboardProfileComponent,
+    DashboardSidebarComponent,
+    DashboardDisplayComponent
+  ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule.forChild(ROUTES)
   ]
 })
 export class DashboardModule { }
